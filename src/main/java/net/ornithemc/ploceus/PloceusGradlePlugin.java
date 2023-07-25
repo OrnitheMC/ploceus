5dffdf9c0b2ab772d1c775e9d901028ceb04972a
Space Walker <spacedoesrs@gmail.com>
2023-07-25 21:55:40 +0200

    log ploceus version when applying plugin

--- src/main/java/net/ornithemc/ploceus/PloceusGradlePlugin.java
+++ src/main/java/net/ornithemc/ploceus/PloceusGradlePlugin.java
@@ -1,16 +1,22 @@
 package net.ornithemc.ploceus;
 
+import java.util.Objects;
+
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.PluginAware;
 
 public class PloceusGradlePlugin implements Plugin<PluginAware> {
 
+	public static final String PLOCEUS_VERSION = Objects.requireNonNullElse(PloceusGradlePlugin.class.getPackage().getImplementationVersion(), "0.0.0+unknown");
+
 	@Override
 	public void apply(PluginAware target) {
 		target.getPlugins().apply(PloceusRepositoryPlugin.class);
 
 		if (target instanceof Project project) {
+			project.getLogger().lifecycle("Ploceus: " + PLOCEUS_VERSION);
+
 			project.getExtensions().create("ploceus", PloceusGradleExtension.class);
 		}
 	}
