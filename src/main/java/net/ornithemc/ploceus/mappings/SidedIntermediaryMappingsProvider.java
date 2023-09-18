4b8d0635b987dec0afedb7f87288e768b82c3d53
Space Walker <spacedoesrs@gmail.com>
2023-09-18 21:06:27 +0200

    add hash string to intermediary provider name if user defines a custom intermediary url this fixes issues where custom intermediaries are written to our default cache file, corrupting the intermediaries for other projects

--- src/main/java/net/ornithemc/ploceus/mappings/SidedIntermediaryMappingsProvider.java
+++ src/main/java/net/ornithemc/ploceus/mappings/SidedIntermediaryMappingsProvider.java
@@ -12,14 +12,28 @@ import net.ornithemc.ploceus.GameSide;
 public abstract class SidedIntermediaryMappingsProvider extends IntermediaryMappingsProvider {
 
 	private GameSide side = GameSide.MERGED;
+	private String defaultUrl = "";
 
 	@Override
 	public String getName() {
-		return side.prefix() + "calamus-" + super.getName();
+		String name = this.side.prefix() + "calamus-" + super.getName();
+
+		String defaultUrl = this.defaultUrl;
+		String url = getIntermediaryUrl().get();
+
+		if (!defaultUrl.equals(url)) {
+			// make sure the name is changed when the user defines a
+			// custom intermediary url, to ensure the default cache
+			// file does not get corrupted with other intermediaries
+			name += "-" + Integer.toHexString(url.hashCode());
+		}
+
+		return name;
 	}
 
 	public void configure(Project project, LoomGradleExtension loom, GameSide side) {
 		this.side = Objects.requireNonNull(side);
+		this.defaultUrl = loom.getIntermediaryUrl().get();
 
 		getIntermediaryUrl()
 			.convention(loom.getIntermediaryUrl())
