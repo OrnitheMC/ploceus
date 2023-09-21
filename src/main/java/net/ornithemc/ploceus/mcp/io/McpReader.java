4941d044c99c406f90f84d875d1e0cab32c409b3
Space Walker <spacedoesrs@gmail.com>
2023-09-21 22:36:33 +0200

    fix mapping visitor order

--- src/main/java/net/ornithemc/ploceus/mcp/io/McpReader.java
+++ src/main/java/net/ornithemc/ploceus/mcp/io/McpReader.java
@@ -30,7 +30,7 @@ import net.fabricmc.mappingio.tree.MemoryMappingTree;
 public class McpReader {
 
 	public static void read(Path intermediary, Path srg, Path mcp, MappingVisitor visitor) throws IOException {
-		read(intermediary, srg, mcp).accept(new MappingDstNsReorder(new MappingSourceNsSwitch(visitor, INTERMEDIARY_NAMESPACE), NAMED_NAMESPACE));
+		read(intermediary, srg, mcp).accept(new MappingSourceNsSwitch(new MappingDstNsReorder(visitor, NAMED_NAMESPACE), INTERMEDIARY_NAMESPACE));
 	}
 
 	public static MappingTreeView read(Path intermediary, Path srg, Path mcp) throws IOException {
