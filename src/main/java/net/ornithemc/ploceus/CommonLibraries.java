2034d28d9086396f05fcfb893f3552f9465a2abf
Space Walker <spacedoesrs@gmail.com>
2024-01-24 11:36:46 +0100

    allow common libraries to apply to ranges that are bound or expand indefinitely to either or both sides

--- src/main/java/net/ornithemc/ploceus/CommonLibraries.java
+++ src/main/java/net/ornithemc/ploceus/CommonLibraries.java
@@ -18,17 +18,17 @@ public class CommonLibraries {
 		this.ploceus = ploceus;
 		this.libraries = new LinkedHashSet<>();
 
-		this.libraries.add(new Library("1.6.4", "org.apache.logging.log4j:log4j-api:2.19.0", "org.apache.logging.log4j:log4j-core:2.19.0"));
-		this.libraries.add(new Library("1.5.2", "com.google.code.gson:gson:2.2.2"));
-		this.libraries.add(new Library("1.5.2", "com.google.guava:guava:14.0"));
-		this.libraries.add(new Library("1.11.2", "it.unimi.dsi:fastutil:7.0.12_mojang"));
-		this.libraries.add(new Library("1.7.5", "commons-codec:commons-codec:1.9"));
-		this.libraries.add(new Library("1.7.10", "org.apache.commons:commons-compress:1.8.1"));
-		this.libraries.add(new Library("1.5.2", "commons-io:commons-io:2.4"));
-		this.libraries.add(new Library("1.5.2", "org.apache.commons:commons-lang3:3.1"));
-		this.libraries.add(new Library("1.7.10", "commons-logging:commons-logging:1.1.3"));
-		this.libraries.add(new Library("1.7.9", "org.apache.httpcomponents:httpcore:4.3.2"));
-		this.libraries.add(new Library("1.7.9", "org.apache.httpcomponents:httpclient:4.3.3"));
+		this.libraries.add(Library.upTo("1.6.4", "org.apache.logging.log4j:log4j-api:2.19.0", "org.apache.logging.log4j:log4j-core:2.19.0"));
+		this.libraries.add(Library.upTo("1.5.2", "com.google.code.gson:gson:2.2.2"));
+		this.libraries.add(Library.upTo("1.5.2", "com.google.guava:guava:14.0"));
+		this.libraries.add(Library.upTo("1.11.2", "it.unimi.dsi:fastutil:8.5.9"));
+		this.libraries.add(Library.upTo("1.7.5", "commons-codec:commons-codec:1.9"));
+		this.libraries.add(Library.upTo("1.7.10", "org.apache.commons:commons-compress:1.8.1"));
+		this.libraries.add(Library.upTo("1.5.2", "commons-io:commons-io:2.4"));
+		this.libraries.add(Library.upTo("1.5.2", "org.apache.commons:commons-lang3:3.1"));
+		this.libraries.add(Library.upTo("1.7.10", "commons-logging:commons-logging:1.1.3"));
+		this.libraries.add(Library.upTo("1.7.9", "org.apache.httpcomponents:httpcore:4.3.2"));
+		this.libraries.add(Library.upTo("1.7.9", "org.apache.httpcomponents:httpclient:4.3.3"));
 	}
 
 	public void addDependencies(String configuration) {
@@ -36,7 +36,10 @@ public class CommonLibraries {
 		Semver mcVersion = new Semver(normalizedMcVersion);
 
 		for (Library library : libraries) {
-			if (mcVersion.compareTo(library.maxMcVersion) <= 0) {
+			boolean minSatisfied = (library.minVersion == null || mcVersion.compareTo(library.minVersion) >= 0);
+			boolean maxSatisfied = (library.maxVersion == null || mcVersion.compareTo(library.maxVersion) <= 0);
+
+			if (minSatisfied && maxSatisfied) {
 				for (String maven : library.maven) {
 					project.getDependencies().add(configuration, maven);
 				}
@@ -44,17 +47,31 @@ public class CommonLibraries {
 		}
 	}
 
-	private class Library {
+	private static class Library {
 
-		private final Semver maxMcVersion;
+		private final Semver minVersion;
+		private final Semver maxVersion;
 		private final String[] maven;
 
-		public Library(String maxMcVersion, String... maven) {
-			this(new Semver(maxMcVersion), maven);
+		public static Library all(String... maven) {
+			return new Library(null, null, maven);
 		}
 
-		public Library(Semver maxMcVersion, String... maven) {
-			this.maxMcVersion = maxMcVersion;
+		public static Library from(String minVersion, String... maven) {
+			return new Library(new Semver(minVersion), null, maven);
+		}
+
+		public static Library upTo(String maxVersion, String... maven) {
+			return new Library(null, new Semver(maxVersion), maven);
+		}
+
+		public static Library between(String minVersion, String maxVersion, String... maven) {
+			return new Library(new Semver(minVersion), new Semver(maxVersion), maven);
+		}
+
+		private Library(Semver minVersion, Semver maxVersion, String... maven) {
+			this.minVersion = minVersion;
+			this.maxVersion = maxVersion;
 			this.maven = maven;
 		}
 	}
