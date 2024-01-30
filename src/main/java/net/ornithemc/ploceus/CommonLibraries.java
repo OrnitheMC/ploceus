aebe0b227ddc80aa7e38ee4a7063ada834b1cba2
Space Walker <spacedoesrs@gmail.com>
2024-01-30 13:59:25 +0100

    fix gson library

--- src/main/java/net/ornithemc/ploceus/CommonLibraries.java
+++ src/main/java/net/ornithemc/ploceus/CommonLibraries.java
@@ -23,7 +23,7 @@ public class CommonLibraries {
 		// this library will ship slf4j api, log4j api and log4j core so we do not need to specify those separately
 		this.libraries.add(Library.all("org.apache.logging.log4j:log4j-slf4j-impl:2.8.1"));
 		this.libraries.add(Library.all("it.unimi.dsi:fastutil:8.5.9"));
-		this.libraries.add(Library.upTo("com.google.code.gson:gson:2.10"));
+		this.libraries.add(Library.all("com.google.code.gson:gson:2.10"));
 
 		this.libraries.add(Library.upTo("1.5.2", "com.google.guava:guava:14.0"));
 		this.libraries.add(Library.upTo("1.7.5", "commons-codec:commons-codec:1.9"));
