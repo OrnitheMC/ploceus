66ee42ad82d054523af0faa04ce07ce50553a3af
Space Walker <spacedoesrs@gmail.com>
2025-09-03 14:09:00 +0200

    fetch library upgrades from meta

--- src/main/java/net/ornithemc/ploceus/LibraryUpgrader.java
+++ src/main/java/net/ornithemc/ploceus/LibraryUpgrader.java
@@ -1,81 +1,31 @@
 package net.ornithemc.ploceus;
 
-import java.util.HashSet;
-import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
-import com.vdurmont.semver4j.Semver;
-
 import net.fabricmc.loom.configuration.providers.minecraft.library.Library;
-import net.fabricmc.loom.configuration.providers.minecraft.library.Library.Target;
 import net.fabricmc.loom.configuration.providers.minecraft.library.LibraryContext;
 import net.fabricmc.loom.configuration.providers.minecraft.library.LibraryProcessor;
 import net.fabricmc.loom.util.Platform;
 
 public class LibraryUpgrader extends LibraryProcessor {
 
-	private final Semver minecraftVersion;
-	private final Set<Library> upgrades;
+	private final PloceusGradleExtension ploceus;
 
 	public LibraryUpgrader(PloceusGradleExtension ploceus, Platform platform, LibraryContext context) {
 		super(platform, context);
 
-		this.minecraftVersion = new Semver(ploceus.normalizedMinecraftVersion());
-		this.upgrades = new HashSet<>();
-
-		if (ploceus.shouldUpgradeLibraries()) {
-			addUpgrades();
-		}
-	}
-
-	private void addUpgrades() {
-		// the slf4j binding for log4j - this version has been tested to work on 1.6, 1.11, 1.12
-		// lower versions of mc not tested because they did not yet ship log4j to begin with
-		addUpgrade("org.slf4j:slf4j-api:2.0.1");
-		addUpgrade("org.apache.logging.log4j:log4j-slf4j2-impl:2.19.0");
-		addUpgrade("org.apache.logging.log4j:log4j-api:2.19.0");
-		addUpgrade("org.apache.logging.log4j:log4j-core:2.19.0");
-		addUpgrade("it.unimi.dsi:fastutil:8.5.9");
-		addUpgrade("com.google.code.gson:gson:2.10");
-
-		// logger-config is needed to make log4j work in versions prior to 13w39a
-		addUpgrade("1.7.0-alpha.13.38.c", "net.ornithemc:logger-config:1.0.0");
-		addUpgrade("1.5.2", "com.google.guava:guava:14.0");
-		addUpgrade("1.7.5", "commons-codec:commons-codec:1.9");
-		addUpgrade("1.7.10", "org.apache.commons:commons-compress:1.8.1");
-		addUpgrade("1.5.2", "commons-io:commons-io:2.4");
-		addUpgrade("1.5.2", "org.apache.commons:commons-lang3:3.1");
-		addUpgrade("1.7.10", "commons-logging:commons-logging:1.1.3");
-		addUpgrade("1.7.9", "org.apache.httpcomponents:httpcore:4.3.2");
-		addUpgrade("1.7.9", "org.apache.httpcomponents:httpclient:4.3.3");
-	}
-
-	private void addUpgrade(String maven) {
-		addUpgrade(null, null, maven);
-	}
-
-	private void addUpgrade(String maxMinecraftVersion, String maven) {
-		addUpgrade(null, maxMinecraftVersion, maven);
-	}
-
-	private void addUpgrade(String minMinecraftVersion, String maxMinecraftVersion, String maven) {
-		boolean minSatisfied = (minMinecraftVersion == null || minecraftVersion.compareTo(new Semver(minMinecraftVersion)) >= 0);
-		boolean maxSatisfied = (maxMinecraftVersion == null || minecraftVersion.compareTo(new Semver(maxMinecraftVersion)) <= 0);
-
-		if (minSatisfied && maxSatisfied) {
-			upgrades.add(Library.fromMaven(maven, Target.COMPILE));
-		}
+		this.ploceus = ploceus;
 	}
 
 	@Override
 	public ApplicationResult getApplicationResult() {
-		return upgrades.isEmpty() ? ApplicationResult.DONT_APPLY : ApplicationResult.MUST_APPLY;
+		return ploceus.shouldUpgradeLibraries() ? ApplicationResult.MUST_APPLY : ApplicationResult.DONT_APPLY;
 	}
 
 	@Override
 	public Predicate<Library> apply(Consumer<Library> dependencyConsumer) {
-		for (Library upgrade : upgrades) {
+		for (Library upgrade : ploceus.getLibraryUpgrades()) {
 			dependencyConsumer.accept(upgrade);
 		}
 
---
+++ src/main/java/net/ornithemc/ploceus/LibraryUpgradesCache.java
@@ -0,0 +1,188 @@
+package net.ornithemc.ploceus;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.gradle.api.Project;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import net.fabricmc.loom.LoomGradleExtension;
+import net.fabricmc.loom.configuration.providers.minecraft.library.Library;
+import net.fabricmc.loom.configuration.providers.minecraft.library.Library.Target;
+
+public class LibraryUpgradesCache {
+
+	private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+
+	private final Project project;
+	private final PloceusGradleExtension ploceus;
+	private final Map<String, List<Library>> libraries;
+	private final Map<String, Path> librariesCache;
+
+	private Integer generation;
+	private String mcVersion;
+
+	public LibraryUpgradesCache(Project project, PloceusGradleExtension ploceus) {
+		this.project = project;
+		this.ploceus = ploceus;
+		this.libraries = new HashMap<>();
+		this.librariesCache = new HashMap<>();
+	}
+
+	private int generation() {
+		if (generation == null) {
+			generation = ploceus.getGeneration().get();
+		}
+
+		return generation;
+	}
+
+	private String mcVersion() {
+		if (mcVersion == null) {
+			mcVersion = ploceus.minecraftVersion();
+		}
+
+		return mcVersion;
+	}
+
+	public List<Library> get() {
+		List<Library> libs = null;
+
+		try {
+			libs = fromMeta();
+		} catch (Exception me) {
+			try {
+				libs = fromCache();
+			} catch (Exception ce) {
+				throw new IllegalStateException("unable to read library upgrades from cache for gen" + generation() + " " + mcVersion(), me);
+			}
+
+			if (libs == null) {
+				throw new IllegalStateException("unable to fetch library upgrades from meta for gen" + generation() + " " + mcVersion() + ", and it is not in the cache", me);
+			}
+		}
+
+		return libs;
+	}
+
+	private List<Library> fromMeta() throws Exception {
+		String metaUrl = Constants.librariesMetaUrl(mcVersion(), generation());
+
+		try (InputStreamReader ir = new InputStreamReader(new URL(metaUrl).openStream())) {
+			JsonArray libsJson = GSON.fromJson(ir, JsonArray.class);
+			List<Library> libs = new ArrayList<>();
+
+			for (JsonElement e : libsJson) {
+				if (!e.isJsonObject()) {
+					continue;
+				}
+
+				JsonObject libJson = e.getAsJsonObject();
+				String name = libJson.get("name").getAsString();
+
+				libs.add(Library.fromMaven(name, Target.COMPILE));
+			}
+
+			libraries.put(mcVersion(), libs);
+
+			try {
+				saveToCache(libs);
+			} catch (Exception e) {
+				project.getLogger().warn("unable to save library upgrades for gen" + generation() + " " + mcVersion() + " to cache", e);
+			}
+
+			return libs;
+		}
+	}
+
+	private List<Library> fromCache() throws Exception {
+		Path libsCache = librariesCache.get(mcVersion());
+
+		if (libsCache == null) {
+			LoomGradleExtension loom = LoomGradleExtension.get(this.project);
+			Path userCache = loom.getFiles().getUserCache().toPath();
+
+			libsCache = userCache.resolve(mcVersion).resolve("library-upgrades.json");
+			librariesCache.put(mcVersion, libsCache);
+		}
+
+		if (!Files.exists(libsCache)) {
+			return null;
+		}
+
+		JsonObject json;
+
+		try (BufferedReader br = Files.newBufferedReader(libsCache)) {
+			json = GSON.fromJson(br, JsonObject.class);
+		}
+
+		List<Library> libs = new ArrayList<>();
+
+		String generation = "gen" + generation();
+		JsonArray libsJson = json.getAsJsonArray(generation);
+
+		if (libsJson == null) {
+			return null;
+		}
+
+		for (JsonElement libJson : libsJson) {
+			String name = libJson.getAsString();
+			Library lib = Library.fromMaven(name, Target.COMPILE);
+
+			libs.add(lib);
+		}
+
+		return libs;
+	}
+
+	private void saveToCache(List<Library> libs) throws Exception {
+		Path libsCache = librariesCache.get(mcVersion());
+
+		if (libsCache == null) {
+			LoomGradleExtension loom = LoomGradleExtension.get(this.project);
+			Path userCache = loom.getFiles().getUserCache().toPath();
+
+			libsCache = userCache.resolve(mcVersion).resolve("library-upgrades.json");
+			librariesCache.put(mcVersion, libsCache);
+		}
+
+		JsonObject json = null;
+
+		if (!Files.exists(libsCache)) {
+			json = new JsonObject();
+		}
+		if (json == null) {
+			try (BufferedReader br = Files.newBufferedReader(libsCache)) {
+				json = GSON.fromJson(br, JsonObject.class);
+			}
+		}
+
+		String generation = "gen" + generation();
+		JsonArray libsJson = new JsonArray();
+
+		json.add(generation, libsJson);
+
+		for (Library lib : libs) {
+			libsJson.add(lib.mavenNotation());
+		}
+
+		Files.createDirectories(libsCache.getParent());
+		
+		try (BufferedWriter bw = Files.newBufferedWriter(libsCache)) {
+			GSON.toJson(json, bw);
+		}
+	}
+}
