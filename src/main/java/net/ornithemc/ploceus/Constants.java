27998a8c7f38204fdcd55c4d61c87fc4d7254474
Space Walker <spacedoesrs@gmail.com>
2025-12-08 21:10:42 +0100

    add legacy fabric maven by default for lwjgl

--- src/main/java/net/ornithemc/ploceus/Constants.java
+++ src/main/java/net/ornithemc/ploceus/Constants.java
@@ -15,6 +15,9 @@ public class Constants {
 	public static final String QUILT_MAVEN_NAME = "Quilt";
 	public static final String QUILT_MAVEN_URL = "https://maven.quiltmc.org/repository/release";
 
+	public static final String LEGACY_FABRIC_MAVEN_NAME = "Legacy Fabric";
+	public static final String LEGACY_FABRIC_MAVEN_URL = "https://maven.legacyfabric.net";
+
 	public static final String FORGE_MAVEN_NAME = "Forge";
 	public static final String FORGE_MAVEN_URL = "https://maven.minecraftforge.net/";
 
